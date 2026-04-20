1a7c356b9111d84176035537088adcf1f04446b5
wch0126whj <wch0126whj@gmail.com>
2026-04-20 16:53:49 +0900

    添加PG3文件夹以课题提交

--- 练习用项目/Project1/calc.cpp
+++
@@ -1,9 +0,0 @@
-#include "calc.h"
-
-
-int Power(int a, int b) {
-	return a + b;
-}
-float Power(float a, float b) {
-	return a + b;
-}
--- 练习用项目/Project1/calc.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-/// <summary>
-/// 计算两个整数的和。
-/// </summary>
-/// <param name="a">第一个加数。</param>
-/// <param name="b">第二个加数。</param>
-/// <returns>两个整数的和。</returns>
-int Power(int a, int b);
-float Power(float a, float b);
