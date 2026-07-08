c98dfd43f9e6317e5adc78f0b95161b5b4982dc2
wch0126whj <wch0126whj@gmail.com>
2026-07-08 12:29:56 +0900

    PG3_01_03

--- PG3/00_00_课题用项目/kamata_PG/main.cpp
+++ PG3/00_00_课题用项目/kamata_PG/main.cpp
@@ -1,43 +1,51 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <windows.h>
 
-int NormalTotalWage(int hour) {
-	return 1072 * hour;
+typedef void (*ResultCallback)(int dice, int userAnswer);
+
+void ShowResult(int dice, int userAnswer) {
+	int diceAnswer = dice % 2;
+
+	printf("\nサイコロの出目は%dです。\n", dice);
+
+	if (diceAnswer == userAnswer) {
+		printf("正解\n");
+	}
+	else {
+		printf("不正解\n");
+	}
 }
 
-int RecursiveHourlyWage(int hour) {
-	if (hour <= 1) {
-		return 100;
-	}
-
-	return RecursiveHourlyWage(hour - 1) * 2 - 50;
-}
-
-int RecursiveTotalWage(int hour) {
-	if (hour <= 0) {
-		return 0;
-	}
-
-	return RecursiveTotalWage(hour - 1) + RecursiveHourlyWage(hour);
+void WaitResult(ResultCallback callback, int dice, int userAnswer) {
+	printf("結果を確認しています...\n");
+	Sleep(3000);
+	callback(dice, userAnswer);
 }
 
 int main(void) {
-	int hour = 1;
+	int dice = 0;
+	int userAnswer = 0;
+	ResultCallback callback = ShowResult;
 
-	printf("hour normal recursive\n");
+	srand((unsigned int)time(NULL));
+	dice = rand() % 6 + 1;
 
-	while (1) {
-		int normalTotal = NormalTotalWage(hour);
-		int recursiveTotal = RecursiveTotalWage(hour);
-
-		printf("%2d %6d %9d\n", hour, normalTotal, recursiveTotal);
-
-		if (recursiveTotal > normalTotal) {
-			printf("\n%d時間働くと、再帰的な賃金体系のほうが儲かります。\n", hour);
-			break;
-		}
-
-		hour++;
+	printf("サイコロの出目が半(奇数)か丁(偶数)かを当ててください。\n");
+	printf("半(奇数)なら1、丁(偶数)なら0を入力してください: ");
+	if (scanf("%d", &userAnswer) != 1) {
+		printf("数字を入力してください。\n");
+		return 0;
 	}
+
+	if (userAnswer != 0 && userAnswer != 1) {
+		printf("0か1を入力してください。\n");
+		return 0;
+	}
+
+	WaitResult(callback, dice, userAnswer);
 
 	return 0;
 }
