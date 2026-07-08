25287f88e8bb700caaf402c870a4937f4078f7ed
wch0126whj <wch0126whj@gmail.com>
2026-07-08 12:38:55 +0900

    PG3_01_04

--- PG3/00_00_课题用项目/kamata_PG/main.cpp
+++ PG3/00_00_课题用项目/kamata_PG/main.cpp
@@ -1,40 +1,25 @@
 #define _CRT_SECURE_NO_WARNINGS
+#include <functional>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include <windows.h>
 
-typedef void (*ResultCallback)(int dice, int userAnswer);
-
-void ShowResult(int dice, int userAnswer) {
-	int diceAnswer = dice % 2;
-
-	printf("\nサイコロの出目は%dです。\n", dice);
-
-	if (diceAnswer == userAnswer) {
-		printf("正解\n");
-	}
-	else {
-		printf("不正解\n");
-	}
-}
-
-void WaitResult(ResultCallback callback, int dice, int userAnswer) {
-	printf("結果を確認しています...\n");
-	Sleep(3000);
-	callback(dice, userAnswer);
+void SetTimeout(std::function<void()> callback, int second) {
+	Sleep(second * 1000);
+	callback();
 }
 
 int main(void) {
 	int dice = 0;
 	int userAnswer = 0;
-	ResultCallback callback = ShowResult;
 
 	srand((unsigned int)time(NULL));
 	dice = rand() % 6 + 1;
 
 	printf("サイコロの出目が半(奇数)か丁(偶数)かを当ててください。\n");
 	printf("半(奇数)なら1、丁(偶数)なら0を入力してください: ");
+
 	if (scanf("%d", &userAnswer) != 1) {
 		printf("数字を入力してください。\n");
 		return 0;
@@ -45,7 +30,21 @@ int main(void) {
 		return 0;
 	}
 
-	WaitResult(callback, dice, userAnswer);
+	auto showResult = [dice, userAnswer]() {
+		int correctAnswer = dice % 2;
+
+		printf("\nサイコロの出目は%dです。\n", dice);
+
+		if (userAnswer == correctAnswer) {
+			printf("正解\n");
+		}
+		else {
+			printf("不正解\n");
+		}
+		};
+
+	printf("結果を確認しています...\n");
+	SetTimeout(showResult, 3);
 
 	return 0;
 }
