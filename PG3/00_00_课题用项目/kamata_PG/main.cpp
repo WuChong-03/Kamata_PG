d4c7e06b8b5cfaf1271feb748afe24e1d5f96320
wch0126whj <wch0126whj@gmail.com>
2026-07-08 15:39:02 +0900

    课题

--- PG3/00_00_课题用项目/kamata_PG/main.cpp
+++ PG3/00_00_课题用项目/kamata_PG/main.cpp
@@ -1,15 +1,34 @@
-#include "Enemy.h"
 #include <stdio.h>
+#include <type_traits>
+
+template <typename T1, typename T2>
+class MinClass {
+public:
+	using ResultType = typename std::common_type<T1, T2>::type;
+
+	ResultType Min(T1 a, T2 b) {
+		if (a < b) {
+			return static_cast<ResultType>(a);
+		}
+
+		return static_cast<ResultType>(b);
+	}
+};
 
 int main(void) {
-	Enemy enemy;
-	int frame = 1;
+	MinClass<int, int> intIntMin;
+	MinClass<float, float> floatFloatMin;
+	MinClass<double, double> doubleDoubleMin;
+	MinClass<int, float> intFloatMin;
+	MinClass<int, double> intDoubleMin;
+	MinClass<float, double> floatDoubleMin;
 
-	while (!enemy.IsFinished()) {
-		printf("---- %dフレーム目 ----\n", frame);
-		enemy.Update();
-		frame++;
-	}
+	printf("int, int: %d\n", intIntMin.Min(114, 514));
+	printf("float, float: %f\n", floatFloatMin.Min(11.4f, 51.4f));
+	printf("double, double: %f\n", doubleDoubleMin.Min(1.14, 5.14));
+	printf("int, float: %f\n", intFloatMin.Min(114, 51.4f));
+	printf("int, double: %f\n", intDoubleMin.Min(114, 5.14));
+	printf("float, double: %f\n", floatDoubleMin.Min(11.4f, 5.14));
 
 	return 0;
 }
